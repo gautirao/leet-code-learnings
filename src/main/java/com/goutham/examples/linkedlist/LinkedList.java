718c9506023939c4cc69e3a08057d82a12d332bf
Goutham Rao <goutham.k.rao@gmail.com>
2023-04-16 07:33:23 +0100

    linked list excercises

---
+++ src/main/java/com/goutham/examples/linkedlist/LinkedList.java
@@ -0,0 +1,106 @@
+package com.goutham.examples.linkedlist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LinkedList {
+    public Node head;
+
+    public void append(int data){
+        // if list is empty start with the head node
+        if (head == null){
+            head = new Node(data);
+            return;
+        }
+        // traverse till end
+        Node current = head;
+        while(current.next != null){
+            current = current.next;
+        }
+        // add a node to the end of the list
+        current.next = new Node(data);
+
+    }
+    //      a ->  b ->  c-> null
+    //  newhead   head
+    public void prepend(int data){
+        Node newHead = new Node(data);
+        newHead.next = head;
+        head = newHead;
+    }
+
+    public void deleteWithValue(int data){
+        // if empty list return
+        if( head == null ){
+                return;
+            }
+       //start traversing by pointing to head node
+       Node current = head;
+        while (current.next!= null){
+            if( current.next.data == data){
+                //      a    ->    b   --> c   --> d
+                //    point b to d if c is to be deleted
+                current.next = current.next.next;
+                return;
+            }
+            // if not move to the next node
+            current= current.next;
+        }
+
+    }
+
+    public void printAll(){
+        if( head == null ){
+            System.out.println("List is empty");
+        }
+        Node current = head;
+        while(current.next != null){
+            System.out.println(current.data);
+            current = current.next;
+        }
+
+    }
+
+    // works only if 1 <= position <= sizeOfList
+    // i.e the position has to be in the list
+    public void deleteNodeAtFromEnd(int position){
+        // fast method . Use two pointers , one fast and another slow
+        // move fast pointer by n nodes ( position ) and move both incrementally until fast pointer is at the end .. then break the chain at the position at slow pointer
+
+        //eg n=2
+       //  a  ->  b  ->  c  ->  d  ->  e  ->  f  -> null
+ //init   slp            fp
+        //  a  ->  b  ->  c  ->  d  ->  e  ->  f  -> null
+//join d with f removing e      slp            fp
+
+        Node slowPointer = head;
+        Node fastPointer = head;
+        int count = 0;
+        while( count < position)
+        {
+            fastPointer = fastPointer.next;
+            count ++;
+        }
+
+        while( fastPointer != null){
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next;
+        }
+        slowPointer.next = slowPointer.next.next;
+
+    }
+
+    public List<Integer> getAllData(){
+        List data = new ArrayList();
+        if(head == null ){
+            return data;
+        }
+        Node current = head ;
+        data.add(current.data);
+        while(current.next!= null){
+            current = current.next;
+            data.add(current.data);
+        }
+        return data;
+    }
+}
---
+++ src/test/java/com/goutham/examples/LinkedListTests.java
@@ -0,0 +1,59 @@
+package com.goutham.examples;
+
+import com.goutham.examples.linkedlist.LinkedList;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class LinkedListTests {
+    private LinkedList list ;
+
+    @BeforeEach
+    public void doEach(){
+        list = new LinkedList();
+    }
+    @Test
+    public void testAppend() {
+    list.append(1);
+    list.append(2);
+   List expected = Arrays.asList(1,2);
+   List actual = list.getAllData();
+   assertEquals(expected,actual);
+
+    }
+    @Test
+    public void testPrepend() {
+        list.prepend(1);
+        list.prepend(2);
+        List expected = Arrays.asList(2,1);
+        List actual = list.getAllData();
+        assertEquals(expected,actual);
+
+    }
+    @Test
+    public void testPrependFail() {
+        list.prepend(1);
+        list.prepend(2);
+        List expected = Arrays.asList(1,2);
+        List actual = list.getAllData();
+        assertNotEquals(expected,actual);
+
+    }
+
+    @Test
+    public void testDeleteAtNthNodeFromEnd() {
+        list.append(1);
+        list.append(2);
+        list.append(3);
+        list.append(4);
+        list.append(5);
+        list.deleteNodeAtFromEnd(2);
+        List expected = Arrays.asList(1,2,3,5);
+        List actual = list.getAllData();
+        assertNotEquals(expected,actual);
+
+    }
+}
